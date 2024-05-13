2b73cddf9e104f36e6a0c3f91aa9f25a4fc09252
rn-byte <ramthakur977+github@gmail.com>
2024-05-13 22:27:27 +0545

    dependency added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_rlt/pages/home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
